ae0bb4d07fbc92c0320b394c772097f06d27f3f7
Luca Weiss <luca.weiss@fairphone.com>
2021-10-05 12:01:05 +0200

    Add support for setting up userdata for automated testing
    This only triggers when a special file in /data is placed which can be
    flashed as part of the device setup in an automated environment.
    
    It should not have any effect on phones that don't have this trigger.
    
    Additionally,
    * Adjust to Android 11: Migrate all changes from system/{core,sepolicy}
    into one FP3 device change.
    * Move to Android.bp.
    
    Issue: FP3-A11#130
    Change-Id: I0b86b95660e82a224848efa54e4aeceb4d05d802
    (cherry picked from commit d92419520ea3507cbf4828dac01bfc7cefe70e41)
    Depends-On: I780e7c112cc39251cb6af9781e8eab4f5aa95c6f

---
+++ automation_setup/automation_setup.sh
@@ -0,0 +1,35 @@
+#!/system/bin/sh
+
+# Check if this script should trigger, exit quietly otherwise.
+if [ ! -f /data/userdata_automation/trigger ]; then
+    echo >&2 "+++ SKIPPING automation setup for ATS. Trigger not set. +++"
+    exit 0
+fi
+
+settings_put() {
+    echo >&2 "[automation_setup] settings put $*"
+    # -w: Wait for the service to be ready, as we're running in early boot
+    cmd -w settings put "$@"
+}
+
+# Output on stderr, to be picked up via stdio_to_kmsg on kmsg.
+echo >&2 "+++ TRIGGERING AUTOMATION SETUP FOR ATS +++"
+
+# Move supplied adb public key to correct directory
+mv -v /data/userdata_automation/adb_keys /data/misc/adb/adb_keys
+chown -v 1000:2000 /data/misc/adb/adb_keys
+
+# Enable adb
+setprop persist.sys.usb.config adb
+settings_put global development_settings_enabled 1
+settings_put global verifier_verify_adb_installs 0
+settings_put global adb_enabled 1
+
+# Skip setupwizard
+settings_put global device_provisioned 1
+settings_put secure user_setup_complete 1
+
+# Remove trigger so we don't run again
+rm -v /data/userdata_automation/trigger
+
+echo >&2 "+++ TRIGGERING AUTOMATION SETUP FOR ATS DONE +++"
