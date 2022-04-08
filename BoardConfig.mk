97f727f6e321f564672e512f5ccdf0f82b6de603
Alexander Martinz <amartinz@shiftphones.com>
2022-04-08 10:01:35 +0200

    axolotl-shiftos: import qcom releasetools as extension
    This allows us to update radio images via OTA.
    
    Change-Id: Icfe51a2d9861e69e44afa97b1b11d73c15f11847
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Kernel
 ifeq ($(TARGET_BUILD_VARIANT),eng)
     TARGET_KERNEL_CONFIG := axolotl_eng_defconfig
 else
@@ -13,3 +14,26 @@ else
         TARGET_KERNEL_CONFIG := axolotl_user_defconfig
     endif
 endif
+
+# Radio
+ADD_RADIO_FILES := true
+AB_OTA_PARTITIONS += \
+    ImageFv \
+    abl \
+    aop \
+    bluetooth \
+    cmnlib \
+    cmnlib64 \
+    devcfg  \
+    dsp \
+    hyp \
+    keymaster \
+    modem \
+    qupfw \
+    storsec \
+    tz \
+    xbl \
+    xbl_config \
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/shift/axolotl/shiftos
