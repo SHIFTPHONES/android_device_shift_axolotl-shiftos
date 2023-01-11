aeb15c24ff4dd107063186366a3e90d0b7f011e7
Alexander Martinz <amartinz@shiftphones.com>
2023-01-11 17:47:29 +0100

    Automation: add additional setup steps
    This matches our manual setup scripts, except for launching wifi
    settings as ATS will autoconfigure it.
    
    Change-Id: I37385a20ac5c2a267f7222c9d7e3c0681fa0b28b
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- automation_setup/automation_setup.sh
+++ automation_setup/automation_setup.sh
@@ -23,9 +23,27 @@ settings_put global development_settings_enabled 1
 settings_put global verifier_verify_adb_installs 0
 settings_put global adb_enabled 1
 
+# Set time to 12 hour format
+settings_put system time_12_24 12
+# Set system locale to English (United States)
+settings_put system system_locales en-US
+
+# Add GPS to location providers
+settings_put secure location_providers_allowed +gps
+
+# Disable automatic OTA updates
+settings_put global ota_disable_automatic_update 1
+
+# Enable BT and WiFi background scanning
+settings_put global ble_scan_always_enabled 1
+settings_put global wifi_scan_always_enabled 1
+
 # Skip setupwizard
 settings_put global device_provisioned 1
 settings_put secure user_setup_complete 1
+
+# Reduce display brightness
+settings_put system screen_brightness 0
 
 # Remove trigger so we don't run again
 rm -v /data/userdata_automation/trigger
@@ -36,7 +54,24 @@ while [ "$(getprop sys.boot_completed)" != "1" ]; do
     sleep 2
 done
 
+# Disable lockscreen
+locksettings clear --old 1234
+locksettings clear --old 12345
+locksettings set-disabled true
+
 # Enable mobile data. Requires service `phone` to be running.
 svc data enable
+
+# Keep device screen on
+svc power stayon true
+
+# Skip Chrome setup
+echo 'chrome --disable-fre' > /data/local/tmp/chrome-command-line
+
+# Decrease media volume
+media volume --stream 0 --set 2
+media volume --stream 2 --set 1
+media volume --stream 3 --set 1
+media volume --stream 4 --set 2
 
 echo >&2 "+++ TRIGGERING AUTOMATION SETUP FOR ATS DONE +++"
