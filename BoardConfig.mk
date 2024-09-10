7466c000dd4936edac8416371b5bfb97a57ba8ea
Alexander Martinz <amartinz@shiftphones.com>
2024-09-10 09:04:13 +0200

    axolotl-shiftos: enable console for non-user builds
    As LineageOS builds userdebug by default we need to extend
    the console activation to non-user builds in order to enable
    UART access for our userdebug builds.
    
    Change-Id: I25e872c6de937547beb96d99babda09ef9b35895
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,11 @@ else
     endif
 endif
 
+# Enable console for non-user builds
+ifneq ($(TARGET_BUILD_VARIANT),user)
+    BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000
+endif
+
 # Security patch level
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
