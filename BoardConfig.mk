f4eb3ac6ca67a732e89733d77986bb4f1cd67d1f
Alexander Martinz <amartinz@shiftphones.com>
2025-04-07 20:09:15 +0200

    axolotl-shiftos: avb: adjust key path
    Change-Id: I4a3f60a085ac0089c95a8c5816fe576f5ac98c65
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,11 +9,7 @@
 # Enable AVB 2.0
 BOARD_AVB_ENABLE := true
 
-ifeq ($(WITH_GMS_MAINLINE),true)
-    PRIVATE_KEY_PATH := vendor/shiftos/private/keys/mainline/avb-4096.pem
-else
-    PRIVATE_KEY_PATH := vendor/shiftos/private/keys/stock/avb-4096.pem
-endif
+PRIVATE_KEY_PATH := vendor/shiftos/private/keys/avb/axolotl-4096.pem
 
 # If private key does not exist, fallback to testkey
 ifeq ($(wildcard $(PRIVATE_KEY_PATH)),)
